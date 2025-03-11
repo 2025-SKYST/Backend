a4878536bb8f81c267fd386e0801f1fbeb3e6edc
SCSC <scsc@SCSC>
2025-03-11 21:07:03 +0900

    CORS 설정 변경

--- mysol/main.py
+++ mysol/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://www.editorialhub.site", "https://editorialhub.site"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
