2b74bbf14e8a069bad14b4c65c967a0eb2175f9b
SCSC <scsc@SCSC>
2025-03-11 19:27:51 +0900

    CORS 요청 해결

--- mysol/main.py
+++ mysol/main.py
@@ -12,24 +12,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://editorialhub.site", "https://www.editorialhub.site", "http://localhost:3000"], 
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 
 
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(DefaultSessionMiddleware)
-
-@app.options("/{full_path:path}")
-async def preflight_handler(request: Request, full_path: str):
-    print(f"🔥 OPTIONS 요청 도착: {request.method} {request.url}")
-    print(f"🔍 요청 헤더: {request.headers}")
-    sys.stdout.flush()
-    return Response(status_code=200)
-
 app.include_router(api_router, prefix="/api")
 
 @app.get("/")
