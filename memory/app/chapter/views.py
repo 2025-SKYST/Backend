f9b259b176ebb4ed3574342af96ea43097128d29
SooinPark1019 <hyperion1019@naver.com>
2025-05-11 05:36:09 +0900

    chapter_id를 넣으면 챕터 조회하는 API 구현

--- memory/app/chapter/views.py
+++ memory/app/chapter/views.py
@@ -7,7 +7,7 @@ from memory.app.chapter.models import Chapter
 from memory.app.chapter.service import ChapterService
 from memory.app.user.models import User
 from memory.app.chapter.dto.requests import ChapterCreateRequest
-from memory.app.chapter.dto.reponses import ChapterProfileResponse, ChapterListResponse
+from memory.app.chapter.dto.reponses import ChapterProfileResponse, ChapterListResponse, ChapterDetailResponse
 from memory.app.user.views import get_current_user_from_header
 
 chapter_router = APIRouter()
@@ -36,3 +36,17 @@ async def get_my_chapters(
     chapters = await chapter_service.get_chapters(user_id=user.id)
     return ChapterListResponse.from_chapters(chapters)
 
+@chapter_router.get("/get/{chapter_id}", status_code=201)
+async def get_chapter_by_id(
+    user: Annotated[User, Depends(get_current_user_from_header)],
+    chapter_service: Annotated[ChapterService, Depends()],
+    chapter_id: int
+) -> ChapterDetailResponse:
+    """
+    단일 chapter의 상세 정보 (메타데이터+이미지 리스트)를 반환합니다.
+    """
+    chapter = await chapter_service.get_chapter(
+        user_id=user.id,
+        chapter_id=chapter_id,
+    )
+    return ChapterDetailResponse.from_chapter(chapter)
