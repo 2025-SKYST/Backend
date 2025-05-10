27747cfb796d77490154f82b8ea880d638fe2d8c
SooinPark1019 <hyperion1019@naver.com>
2025-05-10 19:21:34 +0900

    필요없는 모델/api 제거

--- mysol/api.py
+++ mysol/api.py
@@ -14,16 +14,6 @@ from mysol.app.user.views import user_router
 
 api_router = APIRouter()
 
-api_router.include_router(article_router, prefix="/articles", tags=["articles"])
-api_router.include_router(blog_router, prefix="/blogs", tags=["blogs"])
-api_router.include_router(category_router, prefix="/categories", tags=["categories"])
-api_router.include_router(comment_router, prefix="/comments", tags=["comments"])
-api_router.include_router(draft_router, prefix="/drafts", tags=["drafts"])
-api_router.include_router(image_router, prefix="/images", tags=["images"])
-api_router.include_router(like_router, prefix="/likes", tags=["likes"])
-api_router.include_router(message_router, prefix="/messages", tags=["messages"])
-api_router.include_router(notification_router, prefix="/notifications", tags=["notifications"])
-api_router.include_router(subscription_router, prefix="/subscriptions", tags=["subscriptions"])
 api_router.include_router(user_router, prefix="/users", tags=["users"])
 
 api_router.include_router(user_router, prefix="/users", tags=["users"])
